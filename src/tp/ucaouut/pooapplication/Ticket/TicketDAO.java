d6c34ad6b7c8280023f4bac6b8ac33d7030fa0a8
collins6-cc <elchapocharlie3@gmail.com>
2026-02-07 22:17:43 +0000

    initial commit

---
+++ src/tp/ucaouut/pooapplication/Ticket/TicketDAO.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.ucaouut.pooapplication.Ticket;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+import java.sql.*;
+import java.util.Date;
+import tp.ucaouut.pooapplication.DAO;
+
+public class TicketDAO extends DAO<Ticket> {
+
+    public Ticket genererTicket(int idUtilisateur, int idVoyage) {
+
+        Ticket ticket = new Ticket();
+
+        String sql = """
+            SELECT 
+                u.id AS idUtilisateur, u.nom, u.prenom, u.mail,
+                v.id AS idVoyage, v.destination, v.date_depart,
+                b.id AS idBateau, b.nom AS nomBateau
+            FROM utilisateur u
+            JOIN voyage v ON v.id = ?
+            JOIN bateau b ON v.id_bateau = b.id
+            WHERE u.id = ?
+        """;
+
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, idVoyage);
+            ps.setInt(2, idUtilisateur);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                ticket.setIdUtilisateur(rs.getInt("idUtilisateur"));
+                ticket.setNomUtilisateur(rs.getString("nom"));
+                ticket.setPrenomUtilisateur(rs.getString("prenom"));
+                ticket.setEmailUtilisateur(rs.getString("mail"));
+
+                ticket.setIdVoyage(rs.getInt("idVoyage"));
+                ticket.setDestination(rs.getString("destination"));
+                ticket.setDateDepart(rs.getDate("date_depart"));
+
+                ticket.setIdBateau(rs.getInt("idBateau"));
+                ticket.setNomBateau(rs.getString("nomBateau"));
+
+                ticket.setDateEmission(new Date());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ticket;
+    }
+}
+
---
+++ src/tp/ucaouut/pooapplication/Ticket/TicketView.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.ucaouut.pooapplication.Ticket;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TicketView extends JFrame {
+
+    private JTextArea zoneTicket;
+    private JButton btnImprimer;
+
+    public TicketView(Ticket ticket) {
+
+        setTitle("🎫 Ticket de Voyage");
+        setSize(400, 500);
+        setLocationRelativeTo(null);
+
+        zoneTicket = new JTextArea();
+        zoneTicket.setEditable(false);
+
+        zoneTicket.setText("""
+        ===== TICKET DE VOYAGE =====
+        
+        Client :
+        %s %s
+        Email : %s
+        
+        Voyage :
+        Destination : %s
+        Date départ : %s
+        
+        Bateau :
+        %s
+        
+        Date émission :
+        %s
+        """.formatted(
+                ticket.getNomUtilisateur(),
+                ticket.getPrenomUtilisateur(),
+                ticket.getEmailUtilisateur(),
+                ticket.getDestination(),
+                ticket.getDateDepart(),
+                ticket.getNomBateau(),
+                ticket.getDateEmission()
+        ));
+
+        btnImprimer = new JButton("Imprimer");
+
+        add(new JScrollPane(zoneTicket), BorderLayout.CENTER);
+        add(btnImprimer, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
+
